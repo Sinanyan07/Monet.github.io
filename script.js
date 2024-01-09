25091af43e4768072b30d61a593a248190bdf2e7
Sinanyan07 <lev.sinanyan@gmail.com>
2024-01-09 17:31:21 +0400

    kkk]

--- script.js
+++ script.js
@@ -2,33 +2,43 @@ const parent=document.getElementById('parentDv');
 const child=document.getElementById('childDv');
 const down=document.getElementById('down');
 const upp=document.getElementById('upp');
+const leftDv2=document.querySelector('.leftDv2')
 
 
-// parent.addEventListener('click', function(){
-//     child.style.display = child.style.display='none' ? 'block': 'none';
-//     // parent.onclick=function(){
-//     //     if (down.style.display === 'none') {
-//     //         upp.style.display = 'block';
-//     //       } if(upp.style.display === 'block'){
-//     //         down.style.display = 'none'
-//     //       }
-//     // }
-// })
+
+parent.addEventListener('click', function(){
+    child.style.display = child.style.display==='none' ? 'flex': 'none';
+    down.style.display = down.style.display==='flex' ? 'none': 'flex';
+    upp.style.display = upp.style.display==='none' ? 'flex': 'none';
+})
+
+
+
 
 
 const opener=document.getElementById('menuOpener');
 const opening=document.getElementById('headMenu');
 const closer=document.getElementById('menuCloser');
+
+
+
 opener.onclick=function(){
     opening.style.display='flex';
     opener.style.display='none';
-    document.body.style.setProperty('color', '#00000080');
+    document.body.style.background="#00000080;"
 }
 closer.onclick=function(){
     opening.style.display='none';
     opener.style.display='block';
-  
 }
+
+
+
+
+
+
+
+
 
 
 
@@ -72,3 +82,8 @@ function openAnswerToQuestion(id, imgID) {
     let imgElement = document.getElementById(imgID); 
     imgElement.src = element.style.display === "none" ? './img/arrow-to-down.png' : './img/arrow-to-up.png'; 
 } 
+
+
+function openPageApplication(page) {
+    window.location.href=page
+}
